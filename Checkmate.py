d7377936854b484699f94b2f2cd19e82d7ef9fe4
Esref Ozturk <esrefozturk93@gmail.com>
2014-10-26 22:45:50 +0200

    new methods to Checkmate ( GNUChess interface )

--- Checkmate.py
+++ Checkmate.py
@@ -14,7 +14,7 @@ class Checkmate:
 	
 	def nextmove(self, side, move):
 		'''
-		make next move.
+		Make next move.
 		@param side: string, black|white
 		@param move: string, [a-h][1-8] [a-h][1-8]
 		@return: bool, True|False, whether move is valid.
@@ -22,81 +22,89 @@ class Checkmate:
 	
 	def save(self, filename):
 		'''
-		saves current game state to file.
+		Saves current game state to file.
 		@param filename: absolute path of the file.
 		@return: bool, True|False, whether it is saved successfully.
 		'''
 	
 	def load(self, filename):
 		'''
-		loads current game state from file.
+		Loads current game state from file.
 		@param filename: absolute path of the file.
 		@return: bool, True|False, whether it is loaded successfully.
 		'''
 	
 	def hint(self):
 		'''
-		gives hint as a move.
+		Gives hint as a move.
 		@return: string, [a-h][1-8] [a-h][1-8]
 		'''
 	
 	def addbook(self, filename):
 		'''
-		compiles the given file.
+		Compiles the given file.
 		@param filename: string, absolute path of the file
 		@return: bool, True|False, whether it is added successfully.
 		'''
 	
 	def enablebook(self, enable=True):
 		'''
-		enables or disables book.
+		Enables or disables book.
 		@param enable: bool, True|False
 		@return: bool, True|False, whether operation is successful
 		'''
 	
 	def bookmode(self, mode):
 		'''
-		changes move preference from book.
+		Changes move preference from book.
 		@param mode: string, worst|best|random
 		@return: bool, True|False, whether operation is successful
 		'''
 	
 	def board(self):
 		'''
-		returns current game board.
+		Returns current game board.
 		@return: list of strings, each row is a string, list of rows 
 		'''
 	
 	def history(self):
 		'''
-		returns all moves.
+		Returns all made move history.
 		@return: dictionary of lists, lists contain moves
 		'''
 	
 	def quit(self):
 		'''
-		exits game.
+		Exits game.
 		'''
 	
 	def isfinished(self):
 		'''
-		checks whether game is finished.
+		Checks whether game is finished.
 		@return: bool, True|False
 		'''
 	
 	def undo(self):
 		'''
-		backs up one move in game history.
+		Backs up one move in game history.
 		@return: bool, True|False, whether operation is successful
 		'''
 	def setdepth(self, depth=0):
 		'''
 		Sets the program to look depth moves deep for every search it performs.
 		@depth: int
-		@return: whether operation is successful
+		@return: bool, whether operation is successful
 		'''
-
-
-
-
-
+	
+	def newgame(self):
+		'''
+		Sets up a new game.
+		'''
+	
+	def changemode(self, mode):
+		'''
+		Changes game mode.
+		@param mode: string, single|manual
+		@return: bool, whether operation is successful
+		
+		'''
