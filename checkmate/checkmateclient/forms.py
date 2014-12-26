94ad41d0ec5fee8645f7d5d5f041134cb48213c4
Esref Ozturk <esrefozturk93@gmail.com>
2014-12-27 00:38:41 +0200

    Phase3 main frame is finished with PP

--- checkmate/checkmateclient/forms.py
+++ checkmate/checkmateclient/forms.py
@@ -1,23 +1,45 @@
 from django import forms
 
-COLOR_CHOICES = (   ( 'White' , 'White' ),
-					( 'Black' , 'Black' ),
-				)
+COLOR_CHOICES = (   ( 'White', 'White' ),
+                    ( 'Black', 'Black' ),
+)
 
+DIFFICULTY_CHOICES = (   ( 'easy', 'easy' ),
+                         ( 'hard', 'hard' ),
+)
+MODE_CHOICES = (   ( 'single', 'single' ),
+                   ( 'multi', 'multi' ),
+)
 
-DIFFICULTY_CHOICES = (   ( 'easy' , 'easy' ),
-					( 'hard' , 'hard' ),
-				)
-MODE_CHOICES = (   ( 'single' , 'single' ),
-					( 'multi' , 'multi' ),
-				)
+BOOKMODE_CHOICES = ( ( 'worst', 'worst'  ),
+                     ( 'best','best'),
+                     ('random','random')
+)
+
 
 class startForm(forms.Form):
-	color = forms.ChoiceField( choices = COLOR_CHOICES , initial = 'White' )
-	mode = forms.ChoiceField( choices = MODE_CHOICES, initial = 'single' )
-	difficulty = forms.ChoiceField( choices = DIFFICULTY_CHOICES, initial = 'hard' )
-	book = forms.FileField()
+    color = forms.ChoiceField(choices=COLOR_CHOICES, initial='White')
+    mode = forms.ChoiceField(choices=MODE_CHOICES, initial='single')
+    difficulty = forms.ChoiceField(choices=DIFFICULTY_CHOICES, initial='hard')
+    book = forms.FileField()
+
 
 class connectForm(forms.Form):
-	gameid = forms.IntegerField( min_value=1 , max_value=1000000 ) 
-	color = forms.ChoiceField( choices = COLOR_CHOICES , initial = 'White' )
+    gameid = forms.IntegerField(min_value=1, max_value=1000000)
+    color = forms.ChoiceField(choices=COLOR_CHOICES, initial='White')
+
+class hiddenForm(forms.Form):
+    gameid = forms.IntegerField(widget=forms.HiddenInput())
+    color = forms.ChoiceField(choices=COLOR_CHOICES,widget=forms.HiddenInput())
+
+class playForm(forms.Form):
+    moves = forms.CharField(widget=forms.HiddenInput())
+
+class depthForm(forms.Form):
+    depth = forms.IntegerField(min_value=1, max_value=10)
+
+class modeForm(forms.Form):
+    mode = forms.ChoiceField(choices=MODE_CHOICES)
+
+class bookmodeForm(forms.Form):
+    bookmode = forms.ChoiceField( choices=BOOKMODE_CHOICES )
--- checkmate/checkmateclient/views.py
+++ checkmate/checkmateclient/views.py
@@ -1,40 +1,116 @@
 from django.shortcuts import render
-from checkmateclient.forms import startForm, connectForm
 from socket import *
 from json import *
+
+from checkmateclient.forms import startForm, connectForm,hiddenForm, playForm, depthForm,modeForm
+
+
+
 
 # Create your views here.
 
 def home(request):
-	return render(request,'index.html')
-
+    return render(request, 'index.html')
 
 
 def start(request):
-	form = startForm()
-	context = { 'form' : form }
-	return render(request,'start.html', context )
+    form = startForm()
+    context = {'form': form}
+    return render(request, 'start.html', context)
+
 
 def connect(request):
-	form = connectForm()
-	context = { 'form' : form }
-	return render(request,'connect.html', context )
+    form = connectForm()
+    context = {'form': form}
+    return render(request, 'connect.html', context)
+
+
+def upload_book(book):
+    with open('checkmateclient/books/%s' % book.name, "wb+") as f:
+        for chunk in book.chunks():
+            f.write(chunk)
+
 
 def play(request):
-	data = request.POST
-	if not data:
-		return render(request,'play.html' )
-	if data['operation'] == 'Start':
-		if request.FILES.get('book'):
-			with open('/tmp/asd',"wb+") as myfile:
-				for i in request.FILES.get('book').chunks():
-					myfile.write(i)
 
-		s = socket(AF_INET, SOCK_STREAM)
-		s.connect(("0.0.0.0", 20000))
-		s.send( '{"op":"start" , "color":"%s","params":["%s","%s","%s"]}' % ( data.get('color') , data.get('mode') , data.get('difficulty') , data.get('book') )  )
-		print "------->",loads(s.recv(4096))
-		s.close()
-	elif data['operation'] == 'Connect':
-		pass
-	return render(request,'play.html' )
+    print '-----------------------------------------------------------------'
+    print request.POST
+    print '-----------------------------------------------------------------'
+
+
+    data = request.POST
+
+    if not data:
+        return render(request, 'play.html', {})
+
+    s = socket(AF_INET, SOCK_STREAM)
+    s.connect(("0.0.0.0", 20001))
+
+    if data['operation'] == 'Start':
+        if request.FILES.get('book'):
+            upload_book(request.FILES.get('book'))
+
+        s.send('{"op":"start" , "color":"%s","params":["%s","%s","%s"]}' % (
+            data.get('color'), data.get('mode'), data.get('difficulty'),
+            request.FILES.get('book') and request.FILES.get('book').name ))
+        gameid = loads(s.recv(4096)).get('gameid')
+        color = data.get('color')
+        mode = data.get('mode')
+    else:
+        gameid = data.get('gameid')
+        color = data.get('color')
+
+        s.send('{"op":"connect" , "color":"%s", "gameid":"%s"}' % (data.get('color'), data.get('gameid')))
+        s.recv(4096)
+
+        if data['operation'] == 'Play':
+            moves = data.get('moves')
+            s.send('{"op":"play" , "params":["nextmove","%s","%s"]}' % ( color, moves[:2]+' '+moves[2:] ))
+            s.recv(4096)
+        elif data['operation'] == 'setDepth':
+            depth = data.get('depth')
+            s.send('{"op":"play" , "params":["setdepth","%s"]}' % depth)
+            s.recv(4096)
+        elif data['operation'] == 'changeMode':
+            mode = data.get('mode')
+
+
+
+    # s.send('{"op":"play","params":["getbookmode"]}')
+    #bookmode = loads(s.recv(4096))['bookmode']
+
+    s.send('{"op":"play","params":["getmode"]}')
+    mode = loads(s.recv(4096))['mode']
+
+    s.send('{"op":"play","params":["history"]}')
+    historyx = loads(s.recv(4096))['history']
+    history = []
+    for i in range(len(historyx['Black'])):
+        history.append( [ historyx['White'][i] ,  historyx['Black'][i] ] )
+    if len(historyx['White']) > len(historyx['Black']):
+        history.append( [ historyx['White'][-1] ,  ''] )
+
+    s.send('{"op":"play","params":["getdepth"]}')
+    depth = loads(s.recv(4096))['depth'] or 10
+
+    s.send('{"op":"play","params":["getboard"]}')
+    board = loads(s.recv(4096))['board']
+    for i in range(8):
+        for j in range(8):
+            board[i][j] = [board[i][j], chr(j + ord('a')) + str(8 - i)]
+
+    s.send('{"op":"exit"}')
+    s.recv(4096)
+
+    s.close()
+
+    context = { 'hiddenForm': hiddenForm(initial={'gameid':gameid,'color':color}),
+                'playForm' : playForm(),
+                'board': board,
+                'depthForm': depthForm(initial={'depth': int(depth)}),
+                'history':history,
+                'modeForm':modeForm(inital={'mode':mode}),
+               #'bookmodeform': bookmodeForm(initial={'bookmode':bookmode})
+    }
+
+    return render(request, 'play.html', context)
